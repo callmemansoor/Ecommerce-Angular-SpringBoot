0d154a2d51b7c75d9b81f204b94987013fdc768a
mansoor7866 <pslv786@gmail.com>
2020-08-23 20:32:55 +0530

    search by category id in ui

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -13,14 +14,35 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
 
-  constructor(private productService : ProductService) { }
+  currentCategoryId:number;
+
+  constructor(private productService : ProductService,
+              private route:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   listProducts(){
-    this.productService.getProductList().subscribe(
+    //check if "id" parameter is available
+    const hasCategoryId : boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+
+      //get the "id" param string convert string to a number using the "+" symbol
+      this.currentCategoryId= +this.route.snapshot.paramMap.get('id');
+    
+    }else{
+
+      //not category id available ... default to category 1
+      this.currentCategoryId=1;
+
+    }
+
+    //now get the products for this category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
